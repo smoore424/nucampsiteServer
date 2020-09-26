0e4023a99ef9d24481e23d7935f40fadbdae9d37
Stacey <smoore424@gmail.com>
2020-09-26 12:17:29 -0700

    Week 3 workshop

--- authenticate.js
+++ authenticate.js
@@ -6,6 +6,7 @@ const ExtractJwt = require('passport-jwt').ExtractJwt;
 const jwt = require('jsonwebtoken');
 
 const config = require('./config.js');
+const { NotExtended } = require('http-errors');
 
 exports.local = passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
@@ -38,3 +39,13 @@ exports.jwtPassport = passport.use(
 );
 
 exports.verifyUser = passport.authenticate('jwt', {session: false});
+
+exports.verifyAdmin = ((req, res, next) => {
+    if (req.user.admin) {
+        return next()
+    } else {
+        const err = new Error('You are not authorized to perform this operation!');
+        err.status = 403;
+        return next(err);
+    }
+});
